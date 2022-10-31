3899c0687b8e101d827b74528e2c6ae6919c4bd5
rudinamsubaih <rudina.subaih@gmail.com>
2022-10-31 08:10:36 +0100

    modify the parameters of some experiments, update the description of the dataclass

--- scripts/lib/experiments.py
+++ scripts/lib/experiments.py
@@ -3,16 +3,16 @@ from typing import Optional
 
 
 @dataclass
-class experiment_data:
+class ExperimentData:
     """
     Definition of experimental data
 
-    link: to the data
-    For translations with shift_x anf shift_y
-    Unit: 100 if data are in cm. Otherwise 1
-    inv_y: scalar to reflect y-axis.
-    x_index and y_index are indeces of the array
-    Min and Max are boundary of the straight area
+    - link: to the data
+    - For translations with shift_x and shift_y
+    - Unit: 100 if data are in cm. Otherwise 1
+    - inv_y: scalar to reflect y-axis.
+    - x_index and y_index are indices of the array
+    - Min and Max are boundary of the straight area
     """
 
     link: str
@@ -27,7 +27,7 @@ class experiment_data:
 
 
 EXPERIMENTS = {
-    "BaSiGo_germany_Ziemer": experiment_data(
+    "BaSiGo_germany_Ziemer": ExperimentData(
         link="https://doi.org/10.34735/ped.2013.7",
         shift_x=1,
         shift_y=3,
@@ -35,13 +35,13 @@ EXPERIMENTS = {
         x_index=3,
         y_index=2,
     ),
-    "schoolWDGMainCircle_germany_Wang": experiment_data(
+    "schoolWDGMainCircle_germany_Wang": ExperimentData(
         link="https://doi.org/10.34735/ped.2014.2",
         shift_x=1.25,
         shift_y=1.85,
         inv_y=-1,
     ),
-    "schoolGymBayMainCircle_germany_Wang": experiment_data(
+    "schoolGymBayMainCircle_germany_Wang": ExperimentData(
         link="https://doi.org/10.34735/ped.2014.2",
         shift_x=1.25,
         shift_y=1.85,
@@ -50,13 +50,13 @@ EXPERIMENTS = {
         unit=1,
         inv_y=-1,
     ),
-    "age_china_Cao": experiment_data(
+    "age_china_Cao": ExperimentData(
         link="https://doi.org/10.34735/ped.2017.1",
         shift_x=2.5,
         shift_y=2.5,
         unit=100,
     ),
-    "gender_palestine_Subaih": experiment_data(
+    "gender_palestine_Subaih": ExperimentData(
         link="https://doi.org/10.34735/ped.2018.5",
         shift_x=0,
         shift_y=0,
@@ -66,22 +66,22 @@ EXPERIMENTS = {
         Min=0,
         Max=3.14,
     ),
-    "caserne_germany_Seyfried": experiment_data(
+    "caserne_germany_Seyfried": ExperimentData(
         link="https://doi.org/10.34735/ped.2006.1",
-        shift_x=0,
-        shift_y=2,
+        shift_x=2,
+        shift_y=0,
         y_index=None,
-        unit=100,
+        unit=1,
         inv_y=1,
         Min=-2.0,
         Max=2.0,
     ),
-    "motivation_without_germany_Lukowski": experiment_data(
+    "motivation_germany_lukowski": ExperimentData(
         link="empty",
-        shift_x=1,
-        shift_y=0,
         y_index=None,
         unit=100,
         inv_y=1,
+        Min=0,
+        Max=2,
     ),
 }
