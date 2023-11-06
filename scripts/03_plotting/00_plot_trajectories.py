9d54020d852f1939f800283bfbcd5a68953d14a7
r.subaih <r.subaih@fz-juelich.de>
2023-11-06 08:33:54 +0100

    plot multiple figures together

--- scripts/03_plotting/00_plot_trajectories.py
+++ scripts/03_plotting/00_plot_trajectories.py
@@ -8,6 +8,8 @@ import argparse
 
 import matplotlib.pyplot as plt
 import numpy as np
+from experiments import EXPERIMENTS
+import sqlite3
 
 
 def get_parser_args():
@@ -19,12 +21,19 @@ def get_parser_args():
     parser.add_argument(
         "-p",
         "--path",
-        help="Enter the path of the trajectory file"
+        help="Enter the path of the directory contains the trajectory files"
+    )
+    parser.add_argument(
+        "-n",
+        "--fileName",
+        help="Enter the names of the trajectory files",
+        nargs="+"
     )
     parser.add_argument(
         "-t",
         "--title",
-        help="Enter a title for the figure"
+        help="Enter a title for the figure",
+        nargs="+"
     )
     parser.add_argument(
         "-po",
@@ -37,26 +46,39 @@ def get_parser_args():
 if __name__ == "__main__":
     arg = get_parser_args()
     path = arg.path
-    title = arg.title
+    files = arg.fileName
+    titles = arg.title
     path_output = arg.pathOutput
     fig_name = os.path.basename(os.path.splitext(path)[0])
 
-    fig = plt.figure(figsize=(7, 6))
+    for file, title in zip(files, titles):
+        print("Transforming: %s/%s" % (path, file))
+        file_name = os.path.splitext(file)[0]
+        file_type = os.path.splitext(file)[1]  # extension of the data file
 
-    data = np.loadtxt(path, usecols=(0, 1, 2, 3, 4))
-    plt.plot(data[:, 2], data[:, 3], "bo", markersize=0.3)
+        fig = plt.figure(figsize=(7, 6))
 
-    # plt.xlim(0, 26.8496)
-    # plt.ylim(-1, 1)
+        data = np.loadtxt("%s/%s" % (path, file), usecols=(0, 1, 2, 3, 4))
+        plt.plot(data[:, 2], data[:, 3], "bo", markersize=0.3)
 
-    # plt.vlines(5, -1, 1, colors="r", linestyles="dashed")
-    # plt.vlines(12.85398, -1, 1, colors="r", linestyles="dashed")
-    # plt.vlines(17.85398, -1, 1, colors="r", linestyles="dashed")
+        # plt.xlim(1, 12)
+        # plt.ylim(-2, 6)
+        #
+        # plt.vlines(2.3, -2, 2, colors="r", linestyles="dashed")
+        # plt.vlines(7.48363,  -2, 2, colors="r", linestyles="dashed")
+        # plt.vlines(9.78363,  -2, 2, colors="r", linestyles="dashed")
 
-    plt.xlabel(r"$\rm x~[m]$")
-    plt.ylabel(r"$\rm y~[m]$")
-    plt.title(title)
+        plt.xlim(0, 14.97)
+        plt.ylim(-2, 2)
 
-    plt.savefig("%s/%s.pdf" % (path_output, fig_name))
-    plt.savefig("%s/%s.png" % (path_output, fig_name))
-    plt.close()
+        plt.vlines(2.3, -2, 2, colors="r", linestyles="dashed")
+        plt.vlines(7.48363, -2, 2, colors="r", linestyles="dashed")
+        plt.vlines(9.78363, -2, 2, colors="r", linestyles="dashed")
+
+        plt.xlabel(r"$\rm x~[m]$")
+        plt.ylabel(r"$\rm y~[m]$")
+        plt.title(title)
+
+        plt.savefig("%s/%s.pdf" % (path_output, file_name))
+        plt.savefig("%s/%s.png" % (path_output, file_name))
+        plt.close()
